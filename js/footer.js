20dda6844e064382a34ddf447975bfa0f4e3dd60
wangzhang <wangzhang2016@gmail.com>
2017-07-18 15:52:40 +0800

    1

---
+++ js/footer.js
@@ -0,0 +1,3 @@
+var _base = 'http://codeonline.site/footer.html';
+document.write('<script type="text/javascript" src="' + _base + 'data.js" charset="utf-8"></script>');
+document.write('<script type="text/javascript" src="'+ _base + 'page.js" charset="utf-8"></script>');
